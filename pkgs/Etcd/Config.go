57fc5fef9cbeb6bc9a498c41ef8a8ae889717821
邱波 <kevin_u2@126.com>
2021-12-05 01:36:47 +0800

    add etcd v2

---
+++ pkgs/Etcd/Config.go
@@ -0,0 +1,68 @@
+package Etcd
+
+import (
+	"flag"
+	"github.com/kevinu2/ngo/pkgs/Log"
+	"time"
+)
+
+var ConfigPrefix = "etcdv3"
+
+// Config ...
+type (
+	Config struct {
+		Endpoints []string `json:"endpoints"`
+		CertFile  string   `json:"certFile"`
+		KeyFile   string   `json:"keyFile"`
+		CaCert    string   `json:"caCert"`
+		BasicAuth bool     `json:"basicAuth"`
+		UserName  string   `json:"userName"`
+		Password  string   `json:"-"`
+		// 连接超时时间
+		ConnectTimeout time.Duration `json:"connectTimeout"`
+		Secure         bool          `json:"secure"`
+		// 自动同步member list的间隔
+		AutoSyncInterval time.Duration `json:"autoAsyncInterval"`
+		TTL              int           // 单位：s
+	}
+)
+
+func (config *Config) BindFlags(fs *flag.FlagSet) {
+	fs.BoolVar(&config.Secure, "insecure-etcd", true, "--insecure-etcd=true")
+}
+
+// DefaultConfig 返回默认配置
+func DefaultConfig() *Config {
+	return &Config{
+		BasicAuth:      false,
+		ConnectTimeout: time.Second * 5,
+		Secure:         false,
+	}
+}
+
+// StdConfig ...
+func StdConfig(name string) *Config {
+	return RawConfig(ConfigPrefix + "." + name)
+}
+
+// RawConfig ...
+func RawConfig(key string) *Config {
+	var config = DefaultConfig()
+	if err := UnmarshalKey(key, config); err != nil {
+		Log.Logger().Panic("client etcd parse config panic, err: " + err.Error())
+	}
+	return config
+}
+
+// Build ...
+func (config *Config) Build() (*Client, error) {
+	return newClient(config)
+}
+
+func (config *Config) MustBuild() *Client {
+	client, err := config.Build()
+	if err != nil {
+		Log.Logger().Panic("build etcd client failed, err: " + err.Error())
+	}
+	return client
+}
---
+++ pkgs/Etcd/Etcd.go
@@ -0,0 +1,104 @@
+package Etcd
+
+import (
+	"context"
+	"github.com/kevinu2/ngo/pkgs/Log"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	clientV3 "go.etcd.io/etcd/client/v3"
+)
+
+type etcdv3DataSource struct {
+	propertyKey         string
+	lastUpdatedRevision int64
+	client              *Client
+	// cancel is the func, call cancel will stop watching on the propertyKey
+	cancel context.CancelFunc
+	// closed indicate whether continuing to watch on the propertyKey
+	// closed util.AtomicBool
+
+	logger *Log.Log
+
+	changed chan struct{}
+}
+
+// NewDataSource new a etcdv3DataSource instance.
+// client is the etcdv3 client, it must be useful and should be release by User.
+func NewDataSource(client *Client, key string) DataSource {
+	ds := &etcdv3DataSource{
+		client:      client,
+		propertyKey: key,
+	}
+	go ds.watch()
+	return ds
+}
+
+// ReadConfig ...
+func (s *etcdv3DataSource) ReadConfig() ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	resp, err := s.client.Get(ctx, s.propertyKey)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Count == 0 {
+		return nil, errors.New("empty response")
+	}
+	s.lastUpdatedRevision = resp.Header.GetRevision()
+	return resp.Kvs[0].Value, nil
+}
+
+// IsConfigChanged ...
+func (s *etcdv3DataSource) IsConfigChanged() <-chan struct{} {
+	return s.changed
+}
+
+func (s *etcdv3DataSource) handle(resp *clientV3.WatchResponse) {
+	if resp.CompactRevision > s.lastUpdatedRevision {
+		s.lastUpdatedRevision = resp.CompactRevision
+	}
+	if resp.Header.GetRevision() > s.lastUpdatedRevision {
+		s.lastUpdatedRevision = resp.Header.GetRevision()
+	}
+
+	if err := resp.Err(); err != nil {
+		return
+	}
+
+	for _, ev := range resp.Events {
+		if ev.Type == mvccpb.PUT || ev.Type == mvccpb.DELETE {
+			select {
+			case s.changed <- struct{}{}:
+			default:
+			}
+		}
+	}
+}
+
+func (s *etcdv3DataSource) watch() {
+	ctx, cancel := context.WithCancel(context.Background())
+	s.cancel = cancel
+	rch := s.client.Watch(ctx, s.propertyKey, clientV3.WithCreatedNotify(), clientV3.WithRev(s.lastUpdatedRevision))
+	for {
+		for resp := range rch {
+			s.handle(&resp)
+		}
+		time.Sleep(time.Second)
+
+		ctx, cancel = context.WithCancel(context.Background())
+		if s.lastUpdatedRevision > 0 {
+			rch = s.client.Watch(ctx, s.propertyKey, clientV3.WithCreatedNotify(), clientV3.WithRev(s.lastUpdatedRevision))
+		} else {
+			rch = s.client.Watch(ctx, s.propertyKey, clientV3.WithCreatedNotify())
+		}
+		s.cancel = cancel
+	}
+}
+
+// Close ...
+func (s *etcdv3DataSource) Close() error {
+	s.cancel()
+	return nil
+}
