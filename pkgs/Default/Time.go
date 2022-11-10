2aa11d34b2338132bbd53df44d8ab916aa0b01a3
kevinu2 <62871603+kevinu2@users.noreply.github.com>
2022-11-10 15:09:44 +0800

    Update Time.go

--- pkgs/Default/Time.go
+++ pkgs/Default/Time.go
@@ -4,17 +4,21 @@ const (
 	TimeDefaultFormat = "2006-01-02 15:04:05"
 
 	TimeSimpleHourFormat = "15:04:05"
-
 	TimeSimpleDayFormat = "01-02 15:04:05"
 
-	// TimeUtcFormat "2020-07-02T08:47:29.000Z"
 	TimeUtcFormat = "2006-01-02T15:04:05.000Z"
-	// TimeUtcFormatMicro "2020-07-02T08:47:29.000000Z"
 	TimeUtcFormatMicro = "2006-01-02T15:04:05.000000Z"
-
+	TimeUtcFormatMicroTZ = "2006-01-02T15:04:05.000000+0000"
+	
 	TimeDayFormat = "060102"
 
 	TimeDefaultDayFormat = "2006-01-02"
 
 	DayBySecond = int64(24 * 60 * 60)
 )
+
+
+func Utc2Localtime(utcTime time.Time, location string) time.Time {
+	loc, _ := time.LoadLocation(location) #"Asia/Shanghai"
+	return utcTime.In(loc)
+}
