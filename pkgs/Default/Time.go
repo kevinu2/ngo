f973d0edde8fccad1e1b061021fa0fe76f0a1288
kevinu2 <kevin_u2@126.com>
2022-12-27 16:21:28 +0800

    fix time format

--- pkgs/Default/Time.go
+++ pkgs/Default/Time.go
@@ -4,12 +4,12 @@ const (
 	TimeDefaultFormat = "2006-01-02 15:04:05"
 
 	TimeSimpleHourFormat = "15:04:05"
-	TimeSimpleDayFormat = "01-02 15:04:05"
+	TimeSimpleDayFormat  = "01-02 15:04:05"
 
-	TimeUtcFormat = "2006-01-02T15:04:05.000Z"
-	TimeUtcFormatMicro = "2006-01-02T15:04:05.000000Z"
-	TimeUtcFormatMicroTZ = "2006-01-02T15:04:05.000000+0000"
-	
+	TimeUtcFormat        = "2006-01-02T15:04:05.999Z"
+	TimeUtcFormatMicro   = "2006-01-02T15:04:05.999999Z"
+	TimeUtcFormatMicroTZ = "2006-01-02T15:04:05.999999-0700"
+
 	TimeDayFormat = "060102"
 
 	TimeDefaultDayFormat = "2006-01-02"
